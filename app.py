fee7d5e4ff17cbd7ea3e000661d9b95a87cb797a
Li Mengyuan <143168045+Meng-yuan-Li@users.noreply.github.com>
2025-04-15 22:55:04 +0300

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from Flask and Azure!"
+    return "Hello from Flask and Azure! and now Auto Deploy Works!"
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 8000))
