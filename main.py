11f47741826c3a1e9c7baceac727aa5d30bf85be
Li Mengyuan <143168045+Meng-yuan-Li@users.noreply.github.com>
2025-04-18 15:51:49 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Flask running in Docker!, and it is works!!"
+    return "Hello from Flask running in Docker!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
